d34f996ca394d4436cd38a6976c26e4164f123d8
marsrambler <marsrambler@gmail.com>
2020-05-13 14:55:39 +0800

    v1

---
+++ src/main/java/com/example/soapdemo/CountryEndpoint.java
@@ -0,0 +1,42 @@
+package com.example.soapdemo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ws.server.endpoint.annotation.Endpoint;
+import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
+import org.springframework.ws.server.endpoint.annotation.RequestPayload;
+import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
+
+import io.spring.guides.gs_producing_web_service.GetCityRequest;
+import io.spring.guides.gs_producing_web_service.GetCityResponse;
+import io.spring.guides.gs_producing_web_service.GetCountryRequest;
+import io.spring.guides.gs_producing_web_service.GetCountryResponse;
+
+@Endpoint
+public class CountryEndpoint {
+	private static final String NAMESPACE_URI = "http://spring.io/guides/gs-producing-web-service";
+
+	private CountryRepository countryRepository;
+
+	@Autowired
+	public CountryEndpoint(CountryRepository countryRepository) {
+		this.countryRepository = countryRepository;
+	}
+
+	@PayloadRoot(namespace = NAMESPACE_URI, localPart = "getCountryRequest")
+	@ResponsePayload
+	public GetCountryResponse getCountry(@RequestPayload GetCountryRequest request) {
+		GetCountryResponse response = new GetCountryResponse();
+		response.setCountry(countryRepository.findCountry(request.getName()));
+
+		return response;
+	}
+	
+	@PayloadRoot(namespace = NAMESPACE_URI, localPart = "getCityRequest")
+	@ResponsePayload
+	public GetCityResponse getCity(@RequestPayload GetCityRequest request) {
+		GetCityResponse response = new GetCityResponse();
+		response.setCity(countryRepository.findCountry(request.getName()));
+
+		return response;
+	}
+}
